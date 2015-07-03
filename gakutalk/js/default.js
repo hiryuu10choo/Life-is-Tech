e93aeeb18edb585353c7722d6a65c93ddfd39458
hiryuu10choo <yuta.hrn@gmail.com>
2015-07-03 16:46:41 +0900

    7/3 16:45時点
    ・スムーズスクロール
    ・全体の色変更
    ・SNSアイコン色変更
    ・ロゴ作成

--- gakutalk/js/default.js
+++ gakutalk/js/default.js
@@ -1,7 +1,17 @@
 $(function(){
-
+    
+    //hero画像
+    $(".HE1").show();
+    $(".hero").click(function(){
+        var heNUM = $(this).attr("he");
+        var heID = ".HE" + heNUM;
+        
+        $(".HERO").hide();
+        $(heID).fadeIn(800);
+    });
     
     
+    //メンバー紹介
     $(".fukidashi").click(function(){
         var fuNUM = $(this).attr("fu");
         var fuID = ".me" + fuNUM;
@@ -9,6 +19,15 @@ $(function(){
         $(".member").hide();
         $(fuID).fadeIn(400);
     });
-
+    
+    
+    //スクロール
+    $("a[href^=#]").click(function(){
+        var href = $(this).attr("href");
+        var moveTop = $(href).offset().top;
+        
+        $("body,hrml").animate({scrollTop: moveTop}, 500, "swing");
+        return false;
+    });
         
 });
