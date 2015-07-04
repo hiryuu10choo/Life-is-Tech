b4aab88c288b568b95c8ae7b3578c44df98498f1
hiryuu10choo <yuta.hrn@gmail.com>
2015-07-04 11:48:00 +0900

    7/4 11:47時点
    ・色の変更
    ・説明文
    ・メンバー紹介大幅リニューアル
    ・twitter埋め込み

--- gakutalk/js/default.js
+++ gakutalk/js/default.js
@@ -11,14 +11,43 @@ $(function(){
     });
     
     
+    //date
+    var now = new Date();
+    var m = now.getMonth() + 1;
+    var d = now.getDate();
+    d=23-d;
+    if(m == 7){d=d+31;};//8.23まで有効
+    $("span.date").replaceWith("<span class=date>"+d+"</span>")
+    
+    
     //メンバー紹介
-    $(".fukidashi").click(function(){
-        var fuNUM = $(this).attr("fu");
-        var fuID = ".me" + fuNUM;
-        
-        $(".member").hide();
-        $(fuID).fadeIn(400);
+    $(".me").hover(function(){
+        $(".me").hide();
+        $(this).show();
+        $(".fu").fadeOut(400);
+        $(".mozaiku").animate({opacity: 1.0}, 400, "swing");
+    },function(){
+        $(".fu").fadeIn(400);
+        $(".mozaiku").animate({opacity: 0.0}, 400, "swing", function(){$(".me").show();});
     });
+    
+    $(".me").click(function(){
+        var meNUM = $(this).attr("num");
+        var perID = ".per" + meNUM;
+        $(".per").hide();
+        $(perID).fadeIn(600);
+    });
+    $(".per").click(function(){
+        $(this).fadeOut(600);
+    })
+//    
+//    $(".fukidashi").click(function(){
+//        var fuNUM = $(this).attr("fu");
+//        var fuID = ".me" + fuNUM;
+//        
+//        $(".member").hide();
+//        $(fuID).fadeIn(400);
+//    });
     
     
     //スクロール
